b7c0a957c63d4afdd2afd18da28851e8e0b369c9
rochana_lamp_sys <cs_oruma001@CS.ODU.EDU>
2024-05-29 17:55:59 -0400

    footer-lemos updated

--- components/footer-lemos.js
+++ components/footer-lemos.js
@@ -10,7 +10,7 @@ class Footer_L extends HTMLElement {
         <div class="pt-1 pb-2 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0 small" style="color: var(--light-gray);">Template by <a href="https://rochanaro.github.io/" target="_blank">RochanaRO</a> via <a href="https://github.com/rochanaro" target="_blank">GitHub <i class="mx-1 fab footer-icons fa-github" aria-hidden="true"></i></a> ©2024</p> 
+                    <p class="mb-0 small" style="color: var(--light-gray);">Template by <a href="https://rochanaro.github.io/" target="_blank">RochanaRO</a> via <a href="https://github.com/rochanaro/academic-portfolio-template-lemos" target="_blank">GitHub <i class="mx-1 fab footer-icons fa-github" aria-hidden="true"></i></a> ©2024</p> 
 
                 </div>
             </div>
