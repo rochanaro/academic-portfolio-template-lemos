652cce75e4f2c03e5c624d2c4981ab27f16bc04c
Rochana <rochana.odu@gmail.com>
2025-09-17 14:49:09 -0400

    footer-lemos year updated

--- components/footer-lemos.js
+++ components/footer-lemos.js
@@ -4,13 +4,16 @@ class Footer_L extends HTMLElement {
   }
 
   connectedCallback() {
+    const startYear = 2024;
+    const currentYear = new Date().getFullYear();
+    const yearDisplay = startYear === currentYear ? `${startYear}` : `${startYear}–${currentYear}`;
     this.innerHTML = `
     <div class="footer-lemos-nifme">
 
         <div class="pt-1 pb-2 px-md-5 container-fluid">
             <div class="row">
                 <div class="col-auto col-md-5 align-middle">
-                    <p class="mb-0 small" style="color: #DBE2EF;">Template by <a href="https://rochanaro.github.io/" target="_blank">RochanaRO</a> via <a href="https://github.com/rochanaro/academic-portfolio-template-lemos" target="_blank">GitHub <i class="mx-1 fab footer-icons fa-github" aria-hidden="true"></i></a> ©2024</p> 
+                    <p class="mb-0 small" style="color: #DBE2EF;">Template by <a href="https://rochanaro.github.io/" target="_blank">RochanaRO</a> via <a href="https://github.com/rochanaro/academic-portfolio-template-lemos" target="_blank">GitHub <i class="mx-1 fab footer-icons fa-github" aria-hidden="true"></i></a> ©${yearDisplay}</p> 
 
                 </div>
             </div>
